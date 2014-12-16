a68116750afac7d0f5b67168302cf72e5c9b7adf
Keith Morrison <keithm@infused.org>
2014-12-16 15:16:32 -0800

    scrape other organization types

--- scraper.rb
+++ scraper.rb
@@ -4,22 +4,29 @@ require 'json'
 require 'mechanize'
 require 'turbotlib'
 
-SOURCE_URL = 'https://www.cba.am/en/SitePages/fscfobanks.aspx'
+urls = {
+  banks: 'https://www.cba.am/en/SitePages/fscfobanks.aspx',
+  credit_organizations: 'https://www.cba.am/en/SitePages/fscfocreditorganizations.aspx',
+  insurance_organizations: 'https://www.cba.am/en/SitePages/fscfoinsuranceorganizations.aspx',
+  insurance_brokers: 'https://www.cba.am/en/SitePages/fscfoinsurancebrokers.aspx',
+  lombards: 'https://www.cba.am/en/SitePages/fscfolombards.aspx',
+}
 
-agent = Mechanize.new
-page = agent.get(SOURCE_URL)
+urls.each do |category, url|
+  agent = Mechanize.new
+  page = agent.get(url)
+  page.search('.banks_list_cont .banks_list_desc').each do |bank|
+    data = {
+      company_name: bank.search('a.bank_name').text.strip,
+      organization_head: bank.to_s.match(/<b>Organization Head:<\/b>([^<]*)/m) ? $1.strip : nil,
+      address: bank.to_s.match(/<b>Address:<\/b>([^<]*)/) ? $1.strip : nil,
+      branch: bank.to_s.match(/<b>Branch:<\/b>([^<]*)/) ? $1.strip : nil,
+      contact: bank.to_s.match(/<b>Contact:<\/b>([^<]*)/) ? $1.strip : nil,
+      category: category,
+      source_url: url,
+      sample_date: Time.now
+    }
 
-page.search('.banks_list_cont .banks_list_desc').each do |bank|
-  parts = bank.to_html.split('<br>')
-
-  data = {
-    company_name: bank.search('a').first.text.strip,
-    organization_head: parts[0].split('</b>').last,
-    address: parts[1].split('</b>').last,
-    branch: parts[2].split('</b>').last.gsub(/[^\d]/, ''),
-    source_url: SOURCE_URL,
-    sample_date: Time.now
-  }
-
-  puts JSON.dump(data)
+    puts JSON.dump(data)
+  end
 end
