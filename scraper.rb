0377d2d36f79fe8a0b502c13d6adbf08a92e6d15
Keith Morrison <keithm@infused.org>
2014-12-23 10:48:00 -0800

    extract tpin as separate field

--- scraper.rb
+++ scraper.rb
@@ -16,12 +16,15 @@ urls.each do |category, url|
   agent = Mechanize.new
   page = agent.get(url)
   page.search('.banks_list_cont .banks_list_desc').each do |bank|
+    contact = bank.to_s.match(/<b>Contact:<\/b>([^<]*)/) ? $1.strip : nil
+
     data = {
       company_name: bank.search('a.bank_name').text.strip,
       organization_head: bank.to_s.match(/<b>Organization Head:<\/b>([^<]*)/m) ? $1.strip : nil,
       address: bank.to_s.match(/<b>Address:<\/b>([^<]*)/) ? $1.strip : nil,
       branch: bank.to_s.match(/<b>Branch:<\/b>([^<]*)/) ? $1.strip : nil,
-      contact: bank.to_s.match(/<b>Contact:<\/b>([^<]*)/) ? $1.strip : nil,
+      contact: contact,
+      tpin: contact && contact.match(/TPIN: (\d+)/) && $1,
       category: category,
       source_url: url,
       sample_date: Time.now
